1d8e2979c92b9e8da53d529de2e308e8465d791e
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2022-02-04 14:35:31 +0000

    Switching from Android Studio to Intellij ;D

---
+++ src/main/kotlin/com/mcdev/twitterapikit/response/TweetListResponse.kt
@@ -0,0 +1,34 @@
+package com.mcdev.twitterapikit.response
+
+import com.mcdev.twitterapikit.`object`.Space
+import com.mcdev.twitterapikit.`object`.Tweet
+import com.mcdev.twitterapikit.model.Errors
+import com.mcdev.twitterapikit.model.Includes
+import com.mcdev.twitterapikit.model.Meta
+
+data class TweetListResponse(
+    @SerializedName(value = "data")
+    var `data`: ArrayList<Tweet>?,
+
+    @SerializedName(value = "includes")
+    var includes: Includes?,
+
+    @SerializedName(value = "meta")
+    var meta: Meta?,
+
+    /*Error*/
+    @SerializedName(value = "errors")
+    var errors: ArrayList<Errors>?,
+
+    @SerializedName(value = "detail")
+    var detail: String?,
+
+    @SerializedName(value = "title")
+    var title: String?,
+
+    @SerializedName(value = "type")
+    var type: String?,
+
+    @SerializedName(value = "status")
+    var status: Int?
+)
